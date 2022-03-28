fe2f4e05ead794435de3355c29db4317d5738017
vontph18623 <vontph18623@fpt.edu.vn>
2022-03-28 16:35:18 +0700

    xong doc file excel

--- 999Sys/src/poly/helper/XExcel.java
+++ 999Sys/src/poly/helper/XExcel.java
@@ -14,13 +14,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
+import javax.swing.JFileChooser;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.BuiltinFormats;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.IndexedColors;
@@ -39,18 +43,26 @@ import poly.entity.SanPham;
  */
 public class XExcel {
 
-    public static List<SanPham> readExcel(String excelFilePath) throws Exception {
-        List<SanPham> listSP = new ArrayList<>();
-
+    public static void readExcel(DefaultTableModel model) throws Exception {
+        JFileChooser j = new JFileChooser(new File("E:\\Learn\\Git\\PRO1041_Nhom999\\999Sys\\FileExcel\\"));
+        int r = j.showSaveDialog(null);
+        String excelFilePath;
+            // if the user selects a file
+            if (r == JFileChooser.APPROVE_OPTION)
+            {
+                // set the label to the path of the selected file
+                excelFilePath = j.getSelectedFile().getAbsolutePath();
+            }
+            // if the user cancelled the operation
+            else{
+                return;
+            }
         // Get file
         InputStream inputStream = new FileInputStream(new File(excelFilePath));
-
         // Get workbook
         Workbook workbook = getWorkbook(inputStream, excelFilePath);
-
         // Get sheet
         Sheet sheet = workbook.getSheetAt(0);
-
         // Get all rows
         Iterator<Row> iterator = sheet.iterator();
         while (iterator.hasNext()) {
@@ -59,49 +71,26 @@ public class XExcel {
                 // Ignore header
                 continue;
             }
-
+            Vector data = new Vector();
             // Get all cells
             Iterator<Cell> cellIterator = nextRow.cellIterator();
-
             // Read cells and set value for book object
-            SanPham sp = new SanPham();
             while (cellIterator.hasNext()) {
                 //Read cell
                 Cell cell = cellIterator.next();
                 Object cellValue = getCellValue(cell);
+                data.add(cellValue);
                 if (cellValue == null || cellValue.toString().isEmpty()) {
                     continue;
                 }
-                // Set value for book object
-                int columnIndex = cell.getColumnIndex();
-                switch (columnIndex) {
-                    case 0:
-                        sp.setMaSP(new BigDecimal((double) cellValue).intValue());
-                        break;
-                    case 1:
-                        sp.setTenSanPham((String) getCellValue(cell));
-                        break;
-//                    case 2:
-//                        sp.set(new BigDecimal((double) cellValue).intValue());
-//                        break;
-//                    case 3:
-//                        sp.setPrice((Double) getCellValue(cell));
-//                        break;
-//                    case 4:
-//                        sp.setTotalMoney((Double) getCellValue(cell));
-//                        break;
-                    default:
-                        break;
-                }
-
             }
-            listSP.add(sp);
+            model.addRow(data);
         }
-
+        
         workbook.close();
         inputStream.close();
 
-        return listSP;
+      
     }
 
     // Get Workbook
@@ -132,7 +121,12 @@ public class XExcel {
                 cellValue = evaluator.evaluate(cell).getNumberValue();
                 break;
             case NUMERIC:
-                cellValue = cell.getNumericCellValue();
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    cellValue = cell.getDateCellValue();
+                } else {
+                    String so = String.format("%.0f", cell.getNumericCellValue());
+                    cellValue = so;
+                }
                 break;
             case STRING:
                 cellValue = cell.getStringCellValue();
@@ -187,6 +181,7 @@ public class XExcel {
 
         }
         String pattern = "^[0-9]{1,}$";
+        String pattern2 = "^[0-9]{1,}.{0,1}[0-9]{0,}$";
         for (int i = 0; i < jtable.getRowCount(); i++) {
             row = sheet.createRow(i + 1);
             for (int j = 0; j < jtable.getColumnCount(); j++) {
@@ -194,6 +189,16 @@ public class XExcel {
                 if (jtable.getValueAt(i, j).toString().contains(",")) {
                     cell = row.createCell(j, CellType.NUMERIC);
                     String a = jtable.getValueAt(i, j).toString().replaceAll(",", "");
+                    double b = Double.parseDouble(a);
+                    short format = (short) BuiltinFormats.getBuiltinFormat("#,##0");
+                    CellStyle cellStyleFormatNumber = null;
+                    cellStyleFormatNumber = workbook.createCellStyle();
+                    cellStyleFormatNumber.setDataFormat(format);
+                    cell.setCellStyle(cellStyleFormatNumber);
+                    cell.setCellValue(b);
+                } else if (jtable.getValueAt(i, j).toString().contains(".") && jtable.getValueAt(i, j).toString().matches(pattern2)) {
+                    cell = row.createCell(j, CellType.NUMERIC);
+                    String a = jtable.getValueAt(i, j).toString().substring(0, jtable.getValueAt(i, j).toString().indexOf("."));
                     double b = Double.parseDouble(a);
                     short format = (short) BuiltinFormats.getBuiltinFormat("#,##0");
                     CellStyle cellStyleFormatNumber = null;
@@ -223,7 +228,7 @@ public class XExcel {
 
             }
         }
-        for (int columnIndex = 0; columnIndex <= jtable.getRowCount(); columnIndex++) {
+        for (int columnIndex = 0; columnIndex < jtable.getColumnCount(); columnIndex++) {
             sheet.autoSizeColumn(columnIndex);
         }
     }
